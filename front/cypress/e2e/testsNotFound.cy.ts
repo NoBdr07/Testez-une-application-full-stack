987f70d2cddc6f3beac7476f20e99c3c67803a1f
Noémie <baudry.ddf@gmail.com>
2024-06-14 15:43:58 +0200

    coverage e2e > 80% ok

--- front/cypress/e2e/testsNotFound.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Testing not-found component', () => {
-    it('should display the not found page', () => {
-        cy.visit('/404');
-        cy.get('h1').should('contain.text', 'Page not found !');
-    });
-});
